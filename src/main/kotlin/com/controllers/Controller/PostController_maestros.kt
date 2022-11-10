215ea1babc5597660cafe4c43a131246b9391935
ArielGaleano <g7807710@gmail.com>
2022-11-10 16:48:47 -0600

    creando controller para maestros

--- src/main/kotlin/com/controllers/Controller/PostController_maestros.kt
+++ src/main/kotlin/com/controllers/Controller/PostController_maestros.kt
@@ -8,36 +8,32 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 class PostController_maestros {
-    @RestController
-    @RequestMapping("/post")
-    class PostController_estudiante {
-        @Autowired
-        private val service: PostManagementService? = null
-        @GetMapping(value = ["/greet/{name}"])
-        fun greet(@PathVariable(value = "name") name: String): String {
-            return "Hello, $name"
-        }
+    @Autowired
+    private val service: PostManagementService? = null
 
-        @GetMapping(value = ["/list"])
-        fun list(): ResponseEntity<*> {
-            return ResponseEntity<Any?>(service!!.list(), HttpStatus.OK)
-        }
-
-        @PostMapping("/add")
-        fun add(@RequestBody post: dto_asignatura?): ResponseEntity<*> {
-            return ResponseEntity<Any?>(service!!.add(post!!), HttpStatus.OK)
-        }
-
-        @PutMapping(value = ["/{id}/update"])
-        fun edit(@PathVariable(value = "id") id: String?, @RequestBody post: dto_asignatura?): ResponseEntity<*> {
-            return ResponseEntity<Any?>(service!!.edit(id, post), HttpStatus.OK)
-        }
-
-        @DeleteMapping(value = ["/{id}/delete"])
-        fun delete(@PathVariable(value = "id") id: String?): ResponseEntity<*> {
-            return ResponseEntity<Any?>(service!!.delete(id), HttpStatus.OK)
-        }
+    @GetMapping(value = ["/greet/{name}"])
+    fun greet(@PathVariable(value = "name") name: String): String? {
+        return "Hello, $name"
     }
 
+    @GetMapping(value = ["/list"])
+    fun list(): ResponseEntity<*>? {
+        return ResponseEntity<Any?>(service!!.list(), HttpStatus.OK)
+    }
+
+    @PostMapping("/add")
+    fun add(@RequestBody post: dto_asignatura?): ResponseEntity<*>? {
+        return ResponseEntity<Any?>(service!!.add(post!!), HttpStatus.OK)
+    }
+
+    @PutMapping(value = ["/{id}/update"])
+    fun edit(@PathVariable(value = "id") id: String?, @RequestBody post: dto_asignatura?): ResponseEntity<*>? {
+        return ResponseEntity<Any?>(service!!.edit(id, post), HttpStatus.OK)
+    }
+
+    @DeleteMapping(value = ["/{id}/delete"])
+    fun delete(@PathVariable(value = "id") id: String?): ResponseEntity<*>? {
+        return ResponseEntity<Any?>(service!!.delete(id), HttpStatus.OK)
+    }
 
 }
